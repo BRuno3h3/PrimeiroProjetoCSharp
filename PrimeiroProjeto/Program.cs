8696e9ae9dfaa0d2754101afe5d173b2003fdfdd
BRuno3h3 <bruno.tomasi16@gmail.com>
2024-01-23 21:57:46 -0300

    Primeira etapa do curso de C# da Alura. Adicionar arquivos de projeto.

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,37 @@
+﻿//Screen Sound
+
+string boasVindas = "Sejam bem vindos ao Screen Sound";
+
+void ExibirMensagemdeBoasVindas() 
+{
+    Console.WriteLine(@"
+
+    ░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+    ██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+    ╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+    ░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+    ██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+    ╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+    Console.WriteLine(boasVindas);
+}
+
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine();
+    Console.WriteLine("Digite 1 para registrar uma bando");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para ver a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+   
+    Console.WriteLine();
+
+    Console.Write("Digite sua opção: ");
+    int opcaoEscolhida = int.Parse(Console.ReadLine()!);
+
+
+}
+
+ExibirMensagemdeBoasVindas();
+ExibirOpcoesDoMenu();
